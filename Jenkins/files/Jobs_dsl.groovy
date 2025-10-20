19a022872bc56e59d6cfeeb7251c335f9e33f8c5
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-10-20 23:38:53 +0200

    🔧 Casc and .groovy files created

---
+++ Jenkins/files/Jobs_dsl.groovy
@@ -0,0 +1,6 @@
+folder('Whanos base images') {
+    description('Build base images for the Jenkins instance')
+}
+folder('Projects') {
+    description('Checks projets status and deploys them if necessary')
+}
