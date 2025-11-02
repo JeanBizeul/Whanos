cdd1a180f7bbcea672340456020cbf95b4633d50
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-11-02 19:11:13 +0100

    ✨ Jobs to build Whanos images completed

--- Jenkins/files/images_job_dsl.groovy
+++ Jenkins/files/images_job_dsl.groovy
@@ -5,36 +5,59 @@ folder('Whanos base images') {
 
 freeStyleJob('Whanos base images/whanos-c') {
     disabled(false)
+    wrappers {
+        buildInDocker {
+            dockerfile('Whanos-images/c', 'Dockerfile.base')
+        }
+    }
 }
-
 
 freeStyleJob('Whanos base images/whanos-java') {
     disabled(false)
+    wrappers {
+    buildInDocker {
+            dockerfile('Whanos-images/java', 'Dockerfile.base')
+        }
+    }
 }
 
 freeStyleJob('Whanos base images/whanos-javascript') {
     disabled(false)
+    wrappers {
+        buildInDocker {
+            dockerfile('Whanos-images/javascript', 'Dockerfile.base')
+        }
+    }
 }
 
 freeStyleJob('Whanos base images/whanos-python') {
     disabled(false)
+    wrappers {
+        buildInDocker {
+            dockerfile('Whanos-images/python', 'Dockerfile.base')
+        }
+    }
 }
 
 freeStyleJob('Whanos base images/whanos-befunge') {
     disabled(false)
+    wrappers {
+        buildInDocker {
+            dockerfile('Whanos-images/befunge', 'Dockerfile.base')
+        }
+    }
 }
 
 
 freeStyleJob('Whanos base images/Build all base images') {
     disabled(false)
         publishers {
-        downstream(
-            'whanos-java',
-            'whanos-c',
-            'whanos-javascript',
-            'whanos-python',
-            'whanos-befunge',
-            'FAILURE'
-        )
+        downstream([
+            'Whanos base images/whanos-java',
+            'Whanos base images/whanos-c',
+            'Whanos base images/whanos-javascript',
+            'Whanos base images/whanos-python',
+            'Whanos base images/whanos-befunge'
+        ], 'SUCCESS')
     }
 }
