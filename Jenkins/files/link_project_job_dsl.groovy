bcf2c666acd6653524b3462a054e39d70874de7e
JeanBizeul <jean.bizeul@epitech.eu>
2025-11-02 06:10:52 +0100

    ♻️ Split groovy job dsl file in two One for images building and another for job generation trough project binding

--- Jenkins/files/link_project_job_dsl.groovy
+++ Jenkins/files/link_project_job_dsl.groovy
@@ -1,36 +1,8 @@
-folder('Whanos base images') {
-    description('Build base images for the Jenkins instance')
-}
 folder('Projects') {
     description('Checks projets status and deploys them if necessary')
 }
 
 
-freeStyleJob('Whanos base images/whanos-c') {
-    disabled(false)
-}
-
-
-freeStyleJob('Whanos base images/whanos-java') {
-    disabled(false)
-}
-
-freeStyleJob('Whanos base images/whanos-javascript') {
-    disabled(false)
-}
-
-freeStyleJob('Whanos base images/whanos-python') {
-    disabled(false)
-}
-
-freeStyleJob('Whanos base images/whanos-befunge') {
-    disabled(false)
-}
-
-
-freeStyleJob('Whanos base images/Build all base images') {
-    disabled(false)
-}
 freeStyleJob('link-project') {
     disabled(false)
     parameters {
